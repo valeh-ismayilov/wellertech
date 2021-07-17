bf940b0a91d1bdd9cceaffc3de1e63c37e775d6f
Valeh Ismayilov <82777409+valeh-ismayilov@users.noreply.github.com>
2021-07-18 00:30:08 +0400

    Add files via upload

--- assets/js/mainSection.js
+++ assets/js/mainSection.js
@@ -1,10 +1,16 @@
 __main = `
-<center>
+<center class="flex-app">
     <div class="flex-container">
         <product-el
             img='rtx-3070'
             title='RTX 3070'
             link='rtx-3070'>
+        </product-el>
+
+        <product-el
+            img='rtx-3090'
+            title='RTX 3090'
+            link='rtx-3090'>
         </product-el>
     </div>
 </center>
